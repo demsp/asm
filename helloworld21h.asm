43f2989dc4c747fb3b247055a10505520190b935
demsp <dserp18@gmail.com>
2017-06-20 16:20:58 +0300

    Update helloworld21h.asm

--- helloworld21h.asm
+++ helloworld21h.asm
@@ -1,4 +1,5 @@
-  MODEL	TINY
+;mov ah,09h
+MODEL	TINY
 STACK 100h	
 DATASEG
 	Hellostr DB 'Hello First Step Site $'
@@ -15,5 +16,23 @@ start:
 	int 21h
 	mov ah,04Ch
 	mov al,1h
+	int 21h
+end start	
+
+;mov ah, 40h
+MODEL	TINY
+STACK 256	
+DATASEG
+	Hellostr DB 'Hello First Step Site '
+CODESEG		
+start:	
+	mov ax,@data		
+	mov ds,ax		
+	mov bx,1
+	mov cx,21
+	mov dx,offset Hellostr 
+	mov ah,40h
+	int 21h
+	mov ah, 04Ch		
 	int 21h
 end start	
