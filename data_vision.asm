2ba003d56a3ee96c51d8d06eaf70ad32b9bd8425
demsp <dserp18@gmail.com>
2017-06-20 11:15:48 +0300

    Update and rename data_test.asm to data_vision.asm

---
+++ data_vision.asm
@@ -0,0 +1,16 @@
+ds1 segment para 'data'
+     ff1 dw 3 
+ ds1 ends
+ cs1 segment para 'code'
+  assume ss:nothing,cs:cs1,ds:ds1,es: cs1
+   ;org 100h
+  begin   proc near
+mov al, ds:[256]
+
+add al,2h
+mov bl, 1
+   ret
+   begin endp
+   cs1 ends
+   end begin
+   
