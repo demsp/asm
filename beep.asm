6132033ae041781e5ed2c010527960c05c09e2c7
demsp <dserp18@gmail.com>
2017-06-27 13:39:11 +0300

    Update beep.asm

--- beep.asm
+++ beep.asm
@@ -3,9 +3,20 @@ CSEG SEGMENT
 assume cs:cseg,ds:cseg
 org 100h
 start:  
-    mov bx,0
-mov ax, 0E07h
-int 10h
+  mov bx,0
+  mov ax, 0E07h
+  int 10h
     ret
 CSEG ENDS
 end start
+
+;.com (tiny)
+.model tiny
+       .code
+       ORG    100h
+start: 
+ mov bx,0
+ mov ax, 0E07h
+ int 10h
+       INT    20h
+       END    start
