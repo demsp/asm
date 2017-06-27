e46983936881684fd54ec6f23411b557acd26c52
demsp <dserp18@gmail.com>
2017-06-27 22:44:14 +0300

    Update and rename out_symbol.asm to out_symbol_string.asm

--- out_symbol_string.asm
+++ out_symbol_string.asm
@@ -32,3 +32,23 @@ Write   PROC
 Write   ENDP
 
 End Start
+
+;;string
+MODEL SMALL
+STACK 256
+DATASEG
+	Data1 DB 48h,45h,4Ch,4Ch,4Fh,'$'
+CODESEG
+Start:
+	mov ax,@data 		; установка в ds адреса
+	mov ds,ax 		; сегмента данных
+	mov dx,offset Data1  	; указатель на массив символов
+	mov ah,09h		; вывести строку
+	int 21h
+
+Exit:
+	mov ah,04Ch 	; функция DOS выхода из программы
+	mov al,0h 	; код возврата
+	int 21h 		; Вызов DOS остановка программы
+
+End Start
