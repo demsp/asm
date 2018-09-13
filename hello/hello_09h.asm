a145a00e238a1d99b358752a605120b8b6e3ccde
Dima <dserp18@gmail.com>
2018-09-13 08:12:24 +0300

    Update and rename helloworld21h.asm to hello_09h.asm

--- hello/hello_09h.asm
+++ hello/hello_09h.asm
@@ -19,20 +19,4 @@ start:
 	int 21h
 end start	
 
-;mov ah, 40h
-MODEL	TINY
-STACK 256	
-DATASEG
-	Hellostr DB 'Hello First Step Site '
-CODESEG		
-start:	
-	mov ax,@data		
-	mov ds,ax		
-	mov bx,1
-	mov cx,21
-	mov dx,offset Hellostr 
-	mov ah,40h
-	int 21h
-	mov ah, 04Ch		
-	int 21h
-end start	
+
