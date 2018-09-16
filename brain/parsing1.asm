6a520d633d8afb5bdde30a2640c4911a6dc1d78d
Dima <dserp18@gmail.com>
2018-09-16 14:59:48 +0300

    Update parsing1.asm

--- brain/parsing1.asm
+++ brain/parsing1.asm
@@ -1,31 +1,21 @@
-text segment
-assume cs:text,ds:data
-begin: 
- mov ax,data
- mov ds,ax
- ;Подготовим все необходимое
-  mov dl, stor
-  mov cx,5 ;кол-во циклов
-  mov bx, 0 ;bx-индекс второго эл-та
-cont: 
-cmp dl, 2Bh
-jne next ; нет, выводим +
- mov ah,2
- mov dl, 2Bh
- int 21h
-next: inc bx
-mov dl,stor[bx]
-loop cont
-
-;terminate
- mov ax,4c00h
- int 21h
-text ends
-data segment
-
-stor db 2Bh,2Bh,2Bh,2Bh,2Dh,'$'
- data ends
- stk segment stack
-     db 256 dup (0)
-stk ends
-    end begin          
+model small
+STACK 256
+DATASEG
+ stor DB 43,43,43,45,45,'$' ;+++--
+CODESEG
+start: 
+  mov AX,@data	 
+  mov DS,AX
+  mov cx, 5
+  mov bx, 0
+ tally1:
+  mov ah,2
+  mov dl, stor[bx]
+  int 21h
+  inc bx
+loop tally1 
+   
+ ;terminate: 
+  mov ah,4ch
+  int 21h 
+END start
