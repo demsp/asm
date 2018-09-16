7f8a76863e224f020000eaa0decdf8c87351e92c
Dima <dserp18@gmail.com>
2018-09-16 10:07:26 +0300

    Create ascii_loop.asm

---
+++ output/ascii_loop.asm
@@ -0,0 +1,20 @@
+model small
+STACK 256
+DATASEG
+ CTR     DB 00,'$'
+CODESEG
+start: 
+ MOV CX,256 ;256 итераций
+ LEA DX,CTR  ;Адрес счетчика
+D20:
+ MOV AH,02 ;Функция вывода символа
+ mov dl, CTR
+ INT 21H
+ INC CTR ;Увеличить счетчик
+ LOOP D20 ;Уменьшить CX,
+ ;цикл, если не ноль
+    
+ ;terminate: 
+  mov ah,4ch
+  int 21h 
+END start
