cfc28ecdcc01a8d379780bdce3364f2eac097ae9
Dima <dserp18@gmail.com>
2018-10-07 18:31:39 +0300

    Update string.asm

--- in_out/string.asm
+++ in_out/string.asm
@@ -1,5 +1,5 @@
 ;http://www.cyberforum.ru/asm-beginners/thread380071.html
-
+; ввод строки и вывод на экран
 data segment
      string db 100,100 dup ('$')
 data ends
@@ -10,7 +10,7 @@ start:
     mov ax, data
     mov ds, ax
     
-    mov ah,0ah
+    mov ah,0Ah
     lea dx,string
     int 21h 
     
