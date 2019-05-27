f8578e7831c9b22a3c8c1748cb288735401c583a
demsp <dserp18@gmail.com>
2019-05-27 16:40:03 +0300

    Create kalash3d1_lea.asm

---
+++ in_out/kalash3d1_lea.asm
@@ -0,0 +1,11 @@
+CSEG segment
+ org 100h
+ Begin:
+ mov ah,9
+ ;mov dx,offset Message
+ lea dx,[Message] ;Адрес строки Message: DS:DX 
+ int 21h
+ int 20h
+ Message db 'Hi, Hello, world!!$'
+ CSEG ends
+ end Begin
